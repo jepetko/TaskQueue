e3fa1cd8864572a50a4e8a58e457a21e54433cbf
katarina <golbang.k@gmail.com>
2013-01-07 01:23:24 +0100

    event handler for changing records

--- app/controller/Tasks.js
+++ app/controller/Tasks.js
@@ -8,11 +8,25 @@ Ext.define('TaskQueue.controller.Tasks', {
 
     //////////////// Event Handlers
 
-    onPainted: function() {
-    },
-
     onBeforeLoad: function(store, operation, eOpts ) {
         console.log('before load');
+    },
+
+    onAddRecords: function() {
+        this.addCheckboxListeners( this.getCheckboxes() );
+    },
+
+    onUpdateRecord: function(el, record, newIndex, oldIndex, modifiedFieldNames, modifiedValues, eOpts) {
+        var rotatingTasks = this.getRotatingTasksView();
+        var store = rotatingTasks.getStore();
+        console.log(modifiedFieldNames);
+        if( Ext.Array.contains( modifiedFieldNames, 'done' ) ) {
+            this.setRotatingTasksViewSpecFilters();
+        }
+        store.sync();
+    },
+
+    onCheckboxChanged: function(el, newValue, oldValue, eOpts) {
     },
 
     //////////////// Methods
@@ -21,13 +35,12 @@ Ext.define('TaskQueue.controller.Tasks', {
         console.info('launch');
 
         var rotatingTasks = this.getRotatingTasksView();
-        rotatingTasks.on( { 'painted' : this.onPainted } );
 
         var store = rotatingTasks.getStore();
         this.addStoreListeners(store);
 
         store.generate(50);
-        this.addRotatingTasksViewSpecFilters();
+        this.setRotatingTasksViewSpecFilters();
         store.sync();
     },
 
@@ -42,17 +55,23 @@ Ext.define('TaskQueue.controller.Tasks', {
         return null;
     },
 
-    addStoreListeners: function(store) {
-        store.addListener( 'beforeload', this.onBeforeLoad, this );
+    getCheckboxes: function() {
+        return Ext.ComponentQuery.query('rotatingtasks checkboxfield');
     },
 
-    addRotatingTasksViewSpecFilters: function() {
+    addStoreListeners: function(store) {
+        store.on( 'beforeload', this.onBeforeLoad, this );
+        store.on( 'addrecords', this.onAddRecords, this );
+        store.on( 'updaterecord', this.onUpdateRecord, this );
+    },
+
+    setRotatingTasksViewSpecFilters: function() {
 
         var view = this.getRotatingTasksView();
         var store = view.getStore();
         var ids = [];
         store.each(function (item, index, length) {
-            if( ids.length < 8)
+            if( ids.length < 8 && item.get('done') === false)
                 ids.push(item.get('id'));
         });
 
@@ -63,6 +82,14 @@ Ext.define('TaskQueue.controller.Tasks', {
         });
 
         store.filter(topNItems);
+    },
+
+    addCheckboxListeners: function(checkboxes) {
+        for(var i=0; i<checkboxes.length; i++) {
+            if( checkboxes[i].hasListener('change'))
+                continue;
+            checkboxes[i].on( 'change', this.onCheckboxChanged, this);
+        }
     },
 
     poll: function() {
